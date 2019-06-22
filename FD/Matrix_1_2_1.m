d1e0f2304f9891fb37a79d0dfa2eab303a74dce8
MatNoble <z-matrix@qq.com>
2019-06-22 12:43:46 +0800

    FD optimization poisson

--- FD/Matrix_1_2_1.m
+++ FD/Matrix_1_2_1.m
@@ -1,10 +1,11 @@
 function A = Matrix_1_2_1( n )
 
-        A = eye(n);
-        for i = 1:n-1
-            A(i, i+1) = -1;
-        end
-        A = A + A';
+        A = 2*eye(n);
+        A = A - diag(ones(n-1,1),-1) - diag(ones(n-1,1),1);
+%         for i = 1:n-1
+%             A(i, i+1) = -1;
+%         end
+%         A = A + A';
 
 end
 
--- FD/mesh.m
+++ FD/mesh.m
@@ -1,7 +1,7 @@
 function [ x, y, X, Y ] = mesh( bx, by, h_x, h_y )
 
-        x = bx(1): h_x : bx(2);
-        y = by(1) : h_y : by(2);
+        x = bx(1)+h_x: h_x : bx(2)-h_x;
+        y = by(1)+h_y : h_y : by(2)-h_y;
         [X, Y] = meshgrid(x, y);
 
 end
