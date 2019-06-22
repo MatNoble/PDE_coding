b82c6f8ecf941bdbc9f215e6513fad01c503aff0
MatNoble <z-matrix@qq.com>
2019-06-22 22:09:34 +0800

    FD 1D Heat equations

---
+++ FD/1 D/FD_Heat_1D.m
@@ -0,0 +1,61 @@
+
+function [Nu, Au, k] = FD_Heat_1D(u, f, xx, t, N_b, N_t, p_x, p_t, class)
+        
+        x = RandMesh_1D( xx, N_b, p_x );
+        detalx = diff(x);
+        t = RandMesh_1D(t, N_t, p_t);
+        detalt = diff(t);
+        k = detalt(1)/(detalx(1)^2);
+        A = Matrix( k, N_b, class );
+        
+        Nu = zeros(N_t, N_b);
+        Nu(1, :) = feval(u, 0, x);
+        Nu(:, 1) = feval(u, t, xx(1));
+        Nu(:, end) = feval(u, t, xx(2));
+        
+        [X, T] = meshgrid(x, t);
+        Au = feval(u, T, X);
+        
+        if class == 1 % 5 points Forward_Euler
+            for i = 2:N_t
+                b0 = (detalt(1) * feval(f, t(i-1), x(2:end-1)))';
+                b1 = [Nu(i, 1); b0 ;Nu(i, end)];
+                b2 = Nu(i-1, 2:end-1)';
+                b3 = [0;b2;0];
+                Nu(i, :) = ( A * Nu(i-1, :)' + b1 + b3 )';
+            end
+        elseif class == 2 % 3 points Back_Euler
+            for i = 2:N_t
+                b = zeros(N_b, 1);
+                b(1) = Nu(i,1);
+                b(end) = Nu(i,end);
+                b(2:N_b-1) = (detalx(1)^2)*feval(f, t(i), x(2:N_b-1))' + (1/k)*Nu(i-1,2:N_b-1)';
+                Nu(i,:) = (A \ b)';
+            end
+        elseif class == 3 % C_N
+            for i = 2:N_t
+                b1 = zeros(N_b, 1);
+                b1(2:N_b-1) = detalt(1)*(feval(f, t(i-1), x(2:N_b-1))' + feval(f, t(i), x(2:N_b-1))');
+                b2 = A(:,:,2) * Nu(i-1,:)';
+                b3 = zeros(N_b, 1);
+                b3(1) = Nu(i,1);
+                b3(end) = Nu(i,end);
+                b = b1 + b2 + b3;
+                Nu(i, :) = (A(:,:,1) \ b)';
+            end
+        end
+
+        figure(class)
+        subplot(2,1,1)
+        surfc(X, T, Au)
+        xlabel('X')
+        ylabel('T')
+        title('Analysis Solution')
+        subplot(2,1,2)
+        surfc(X, T, Nu)
+        xlabel('X')
+        ylabel('T')
+        title('Numerical Solution')
+        
+        
+end
--- FD/1 D/Matrix.m
+++ FD/1 D/Matrix.m
@@ -1,21 +1,40 @@
-function A = Matrix( k, n )
+function A = Matrix( k, n, class )
 
-        A = ((6*k^2-5*k)/4) * eye(n);
-        for i = 1:n-1
-            A(i, i+1) = (2*k*(2-3*k))/3;
-            if i < n-1
-                A(i, i+2) = -(k*(1-6*k))/12;
-            end
+        if class == 1
+            A = ((6*k^2-5*k)/2) * eye(n);
+            A = A + ((2*k*(2-3*k))/3) * diag(ones(n-1,1),1) + ((2*k*(2-3*k))/3) * diag(ones(n-1,1),-1);
+            A = A + (-(k*(1-6*k))/12) * diag(ones(n-2,1),2) + (-(k*(1-6*k))/12) * diag(ones(n-2,1),-2);
+            
+            A(2, :) = 0;           
+            A(2, 2) = -2*k;
+            A(2, [1 , 3]) = k;
+            
+            A(n-1, :) = 0;
+            A(n-1, [n-2, n]) = k;
+            A(n-1, n-1) = -2*k;
+            
+            A(1, :) = 0;
+            A(end, :) = 0;
+        elseif class == 2
+            A = (2+1/k) * eye(n);
+            A = A - diag(ones(n-1,1),1) - diag(ones(n-1,1),-1);
+            A(1,:) = 0;
+            A(end,:) = 0;
+            A(1,1) = 1;
+            A(end,end) = 1;
+        elseif class == 3
+            A(:,:,1) = (2+2*k)*eye(n);
+            A(:,:,1) = A(:,:,1) - k*diag(ones(n-1,1),1) - k*diag(ones(n-1,1),-1);
+            A(1,:,1) = 0;
+            A(1,1,1) = 1;
+            A(end,:,1) = 0;
+            A(end,end,1) = 1;
+            
+            A(:,:,2) = (2-2*k)*eye(n);
+            A(:,:,2) = A(:,:,2) + k*diag(ones(n-1,1),1) + k*diag(ones(n-1,1),-1);
+            A(1,:,2) = 0;
+            A(end,:,2) = 0;
         end
-        A = A + A';
-        A(2, :) = 0;
-        A(n-1, :) = 0;
-        A(2, 2) = -2*k;
-        A(2, [1 , 3]) = k;
-        A(n-1, [n-2, n]) = k;
-        A(n-1, n-1) = -2*k;
-        A(1, :) = 0;
-        A(end, :) = 0;
 
 end
 
