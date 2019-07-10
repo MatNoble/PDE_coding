0a986d295db11c4f46fd63301fbae4c5ee731282
MatNoble <z-matrix@qq.com>
2019-07-10 13:37:20 +0800

    hui zi xing 1

--- FD/2D/Poisson_2D_FD_rectangle_dirichlet.m
+++ FD/2D/Poisson_2D_FD_rectangle_dirichlet.m
@@ -6,47 +6,49 @@ function [ N_u ] = Poisson_2D_FD_rectangle_dirichlet( bx, by, N_x, N_y, Au, f, M
         % inner mesh
         h_x = (bx(2) - bx(1))/N_x;
         h_y = (by(2) - by(1))/N_y;
-        s = h_x * h_y;
+%         s = h_x * h_y;
         [ x, y, X, Y, XX, YY ] = mesh( bx, by, h_x, h_y );
         
         % figure mesh
         z_mesh( bx, by, N_x, N_y, X, Y, XX, YY, Mesh )
-              
-        % number of unkonw nodes
-        n_x = N_x-1;
-        n_y = N_y-1;
-        if Mesh == 1
-            N_m = (n_x)*(n_y);
-        elseif Mesh == 2
-            N_m = (n_x)*(n_y) - N_x*N_y/4;
-        end
-        index_x = N_x/2-1;
-        index_y = N_y/2-1;
         
-        % generate_P_matrix
-        P = generate_P_matrix( N_m, x, y, n_x, n_y, index_x, index_y, Mesh );
-
-        % A
-        A = assembly_A_matrix( N_x, N_y, h_x, h_y, index_x, index_y, Mesh );
-        
-        % b
-        b = assembly_b_vector( N_m, f, P );
-
-        % treat dirichlet boundary conditions
-        b = treat_dirichlet_boundary_conditions( Au, P, b, bx, by, h_x, h_y, Mesh );
-
-        % linear equations
-        N_u = A \ b;
-        A_u = Au(P(1,:), P(2,:))';
-        
-        % error 
-%         Error_Max = norm((N_u-A_u), inf);
-%         fprintf('Error_Max_norm: %g\n',Error_Max)
-%         Ones = ones(1, N_m);
-%         Error_L2 = sqrt(Ones * (s*(N_u-A_u).^2));
-%         fprintf('Error_L2_norm: %g',Error_L2)
-        
-        % error mesh
-        error_mesh( Au, N_m, N_u, A_u, N_x, N_y, XX, YY, index_y, Mesh)
+        N_u = 0;
+%               
+%         % number of unkonw nodes
+%         n_x = N_x-1;
+%         n_y = N_y-1;
+%         if Mesh == 1
+%             N_m = (n_x)*(n_y);
+%         elseif Mesh == 2
+%             N_m = (n_x)*(n_y) - N_x*N_y/4;
+%         end
+%         index_x = N_x/2-1;
+%         index_y = N_y/2-1;
+%         
+%         % generate_P_matrix
+%         P = generate_P_matrix( N_m, x, y, n_x, n_y, index_x, index_y, Mesh );
+% 
+%         % A
+%         A = assembly_A_matrix( N_x, N_y, h_x, h_y, index_x, index_y, Mesh );
+%         
+%         % b
+%         b = assembly_b_vector( N_m, f, P );
+% 
+%         % treat dirichlet boundary conditions
+%         b = treat_dirichlet_boundary_conditions( Au, P, b, bx, by, h_x, h_y, Mesh );
+% 
+%         % linear equations
+%         N_u = A \ b;
+%         A_u = Au(P(1,:), P(2,:))';
+%         
+%         % error 
+% %         Error_Max = norm((N_u-A_u), inf);
+% %         fprintf('Error_Max_norm: %g\n',Error_Max)
+% %         Ones = ones(1, N_m);
+% %         Error_L2 = sqrt(Ones * (s*(N_u-A_u).^2));
+% %         fprintf('Error_L2_norm: %g',Error_L2)
+%         
+%         % error mesh
+%         error_mesh( Au, N_m, N_u, A_u, N_x, N_y, XX, YY, index_y, Mesh)
               
 end
--- FD/2D/z_mesh.m
+++ FD/2D/z_mesh.m
@@ -1,7 +1,46 @@
 function [ ] = z_mesh( bx, by, N_x, N_y, X, Y, XX, YY, Mesh )
 
         figure(Mesh)
-        if Mesh == 2
+        if Mesh == 3
+            
+            XX1 = XX(:,1:N_x/4+1);
+            YY1 = YY(:,1:N_x/4+1);
+            plot(XX1,YY1,'k-'),hold on
+            plot(XX1',YY1','k-'),hold on
+            plot(XX1(2:N_y/4,end-1:end)',YY1(2:N_y/4,end-1:end)','r-','linewidth',1.5),hold on
+            plot(XX1(end-N_y/4+1:end-1,end-1:end)',YY1(end-N_y/4+1:end-1,end-1:end)','r-','linewidth',1.5),hold on
+            plot(XX1(2:end-1,2:end-1),YY1(2:end-1,2:end-1),'rx-','linewidth',1.5,'MarkerSize',10),hold on
+            plot(XX1(2:end-1,2:end-1)',YY1(2:end-1,2:end-1)','rx-','linewidth',1.5,'MarkerSize',10),hold on
+            
+            XX2 = XX(1:N_y/4+1,N_x/4+1:end-N_x/4);
+            YY2 = YY(1:N_y/4+1,N_x/4+1:end-N_x/4);
+            plot(XX2,YY2,'k-'),hold on
+            plot(XX2',YY2','k-'),hold on
+            plot(XX2(2:end-1,:),YY2(2:end-1,:),'rx-','linewidth',1.5,'MarkerSize',10),hold on
+            plot(XX2(2:end-1,:)',YY2(2:end-1,:)','rx-','linewidth',1.5,'MarkerSize',10),hold on
+            
+            XX3 = XX(end-N_y/4:end,N_x/4+1:end-N_x/4);
+            YY3 = YY(end-N_y/4:end,N_x/4+1:end-N_x/4);
+            plot(XX3,YY3,'k-'),hold on
+            plot(XX3',YY3','k-'),hold on
+            plot(XX3(2:end-1,:),YY3(2:end-1,:),'rx-','linewidth',1.5,'MarkerSize',10),hold on
+            plot(XX3(2:end-1,:)',YY3(2:end-1,:)','rx-','linewidth',1.5,'MarkerSize',10),hold on
+            
+            XX4 = XX(:,end-N_x/4:end);
+            YY4 = YY(:,end-N_x/4:end);
+            plot(XX4,YY4,'k-'),hold on
+            plot(XX4',YY4','k-'),hold on
+            plot(XX4(2:N_y/4,1:2)',YY4(2:N_y/4,1:2)','r-','linewidth',1.5),hold on
+            plot(XX4(end-N_y/4+1:end-1,1:2)',YY4(end-N_y/4+1:end-1,1:2)','r-','linewidth',1.5),hold on
+            plot(XX4(2:end-1,2:end-1),YY4(2:end-1,2:end-1),'rx-','linewidth',1.5,'MarkerSize',10),hold on
+            plot(XX4(2:end-1,2:end-1)',YY4(2:end-1,2:end-1)','rx-','linewidth',1.5,'MarkerSize',10),hold off
+            
+            
+%             plot(X,Y,'rx-','linewidth',1.5,'MarkerSize',10),hold on
+%             plot(X',Y','rx-','linewidth',1.5,'MarkerSize',10),hold off
+            title('Mesh')
+        elseif Mesh == 2
+            % Mesh node
             N_X = N_x + 2;
             N_Y = N_y + 2;
             XX1(1:N_Y/2,:) = XX(1:N_Y/2,:);
@@ -16,6 +55,8 @@ function [ ] = z_mesh( bx, by, N_x, N_y, X, Y, XX, YY, Mesh )
             plot(XX1(1:N_Y/2,:)',YY1(1:N_Y/2,:)','k-'),hold on
             plot(XX1(N_Y/2+1:N_Y,1:N_X/2),YY1(N_Y/2+1:N_Y,1:N_X/2),'k-'),hold on
             plot(XX1(N_Y/2+1:N_Y,1:N_X/2)',YY1(N_Y/2+1:N_Y,1:N_X/2)','k-'),hold on
+            
+            % finite diffierence node
             
             X1(1:N_y/2,:) = X(1:N_y/2,:);
             X1(N_y/2+1:N_y,:) = X(N_y/2:N_y-1,:);
